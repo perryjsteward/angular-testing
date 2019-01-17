c3c8a91a7883c5f6a3d39f0a06717360648f619f
Perry Steward <perryjsteward@gmail.com>
2019-01-17 20:58:35 +0000

    added user service tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,4 +1,4 @@
-import { TestBed, async } from '@angular/core/testing';
+import { async, TestBed } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
 describe('AppComponent', () => {
@@ -16,16 +16,5 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
-  }));
-  it(`should have as title 'angular-testing'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-testing');
-  }));
-  it('should render title in a h1 tag', async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-testing!');
   }));
 });
---
+++ src/app/services/users/users.service.spec.ts
@@ -0,0 +1,19 @@
+import { TestBed, inject } from '@angular/core/testing';
+import { UsersService } from './users.service';
+
+
+describe('UsersService', () => {
+  let usersService: UsersService; // Add this
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      providers: [UsersService]
+    });
+
+    usersService = TestBed.get(UsersService); // Add this
+  });
+
+  it('should be created', () => { // Remove inject()
+    expect(usersService).toBeTruthy();
+  });
+});
